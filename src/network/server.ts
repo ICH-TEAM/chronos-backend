552ae555d76d8f17cf62171d4b2c8849e717e240
andr33wD <andree.anchi@chazki.com>
2022-05-14 20:51:50 -0500

    feat : Base code added

---
+++ src/network/server.ts
@@ -0,0 +1,100 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import pino, { HttpLogger } from 'express-pino-logger'
+
+import { applyRoutes } from './router'
+
+const PORT = (process.env.PORT as string) || 1996
+
+class Server {
+  #app: express.Application
+  #connection: mongoose.Connection | undefined
+  #log: HttpLogger
+
+  constructor() {
+    this.#app = express()
+    this.#log = pino({
+      transport: {
+        target: 'pino-pretty',
+        options: {
+          colorize: true
+        }
+      }
+    })
+    this.#config()
+  }
+
+  #config() {
+    this.#app.use(cors())
+    this.#app.use(express.json())
+    this.#app.use(express.urlencoded({ extended: false }))
+    this.#app.use(
+      (
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+      ) => {
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+        res.header('Access-Control-Allow-Origin', '*')
+        res.header(
+          'Access-Control-Allow-Headers',
+          'Authorization, Content-Type'
+        )
+        res.header('x-powered-by', 'Simba.js')
+        next()
+      }
+    )
+    applyRoutes(this.#app)
+  }
+
+  async #mongo(): Promise<void> {
+    this.#connection = mongoose.connection
+    const connection = {
+      keepAlive: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+    this.#connection.on('connected', () => {
+      this.#log.logger.info('Mongo connection established.')
+    })
+    this.#connection.on('reconnected', () => {
+      this.#log.logger.info('Mongo connection reestablished')
+    })
+    this.#connection.on('disconnected', () => {
+      this.#log.logger.info('Mongo connection disconnected')
+      this.#log.logger.info('Trying to reconnected to Mongo...')
+      setTimeout(() => {
+        mongoose.connect(process.env.MONGO_URI as string, {
+          ...connection,
+          connectTimeoutMS: 3000,
+          socketTimeoutMS: 3000
+        })
+      }, 3000)
+    })
+    this.#connection.on('close', () => {
+      this.#log.logger.info('Mongo connection closed')
+    })
+    this.#connection.on('error', (e: Error) => {
+      this.#log.logger.info('Mongo connection error:')
+      this.#log.logger.error(e)
+    })
+    await mongoose.connect(process.env.MONGO_URI as string, connection)
+  }
+
+  public start(): void {
+    this.#app.listen(PORT, () => {
+      this.#log.logger.info(`Server running at port ${PORT}`)
+    })
+
+    try {
+      this.#mongo()
+    } catch (e) {
+      this.#log.logger.error(e)
+    }
+  }
+}
+
+const server = new Server()
+
+export { server as Server }
